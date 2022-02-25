df33b6e76983575ce3ac91ee3dd36b81b34903e2
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-02-25 14:42:15 -0500

    added assignment 2 sol and spec pdf

--- Racket/Assignment 2/2.7.rkt
+++ Racket/Assignment 2/2.7.rkt
@@ -1,1 +1,13 @@
 #lang racket
+(require 2htdp/image)
+( define ( random-color ) ( color ( rgb-value ) ( rgb-value ) ( rgb-value ) ))
+( define ( rgb-value ) ( random 256 ) )
+
+( define ( create-star points radius )
+   ( radial-star points (/ radius 2) radius "solid" ( random-color)))
+
+( define ( generate-stars points radius )
+   ( cond
+     [( eq? radius 0)(create-star points radius)]
+     [else ( overlay
+           ( generate-stars points ( - radius 100 ))( create-star points radius))]))
