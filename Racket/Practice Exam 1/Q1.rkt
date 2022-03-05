af5ee6c464329a01ec91c7a564f27715b66e2f2f
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-03-05 18:11:06 -0500

    tele

--- Racket/Practice Exam 1/Q1.rkt
+++ Racket/Practice Exam 1/Q1.rkt
@@ -11,3 +11,9 @@
 
 ;;; Question 2
 
+( define ( grid grid-side )
+   ( define w 'white )( define b 'blue )( define r 'red ) ( define v 'violet )
+   ( define tile-size ( / grid-side 3 ))
+   ( above ( beside ( tile tile-size r w )( tile tile-size r w )( tile tile-size r w ))
+           ( beside ( tile tile-size v w )( tile tile-size v w )( tile tile-size v w ))
+           ( beside ( tile tile-size b w )( tile tile-size b w )( tile tile-size b w ))))
