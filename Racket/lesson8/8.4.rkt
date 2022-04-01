f82d62fe8fff3e9ae2ed2765b7f2f2c8d3a701a6
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-03-31 21:48:43 -0400

    ~~~

--- Racket/lesson8/8.4.rkt
+++ Racket/lesson8/8.4.rkt
@@ -1,1 +1,21 @@
 #lang racket
+
+( require 2htdp/image )
+
+
+( define ( foldright operator value list )
+   ( cond
+      ( ( empty? list ) value )
+      ( else
+        ( operator ( car list ) ( foldright operator value ( cdr list ) ) )
+        )
+      )
+   )
+
+
+( define ( arrow n c )
+   ( define part ( square 50 "solid" c ) )
+   ( define head ( rotate 30 ( triangle 70 "solid" c ) ) )
+   ( define stem ( foldr beside empty-image ( make-list ( - n 1 ) part ) ) )
+   ( beside stem head )
+   )
