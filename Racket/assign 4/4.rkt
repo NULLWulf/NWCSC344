b81e2e1d8b3b03890acbf7b445c2d22c49f0f11e
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2022-04-01 15:00:14 -0400

    Added q4

--- Racket/assign 4/4.rkt
+++ Racket/assign 4/4.rkt
@@ -1,1 +1,28 @@
 #lang racket
+
+( define ( assoc o a-list )
+   ( cond
+      ( ( empty? a-list ) '() )
+      ( ( eq? o ( caar a-list ) )( car a-list))
+      ( else ( assoc o ( cdr a-list ) ) )
+      )
+   )
+
+
+;;;; Uses a-list from Question 3
+
+( define ( a-list l-one l-two )
+   ( cond 
+      ( ( empty? l-one ) '() )
+      ( else ( cons ( cons ( car l-one) ( car l-two) )
+                    ( a-list ( cdr l-one ) ( cdr l-two ) ) )
+             )
+      )
+   )
+( define al1
+   ( a-list '(one two three four ) '(un deux trois quatre ) )
+   )
+
+( define al2
+   ( a-list '(one two three) '( (1) (2 2) (3 3 3) ) ) )
+
